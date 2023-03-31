f156c8f1002f79e04b3adc23d74592d92310d62c
Sebastian <sebastianfoulger@hotmail.co.uk>
2023-03-31 17:28:02 +0100

    Small changes

--- animation/animation.py
+++ animation/animation.py
@@ -17,11 +17,11 @@ skip_frames = 1 # We keep one in every skip_frames frames. i.e. if it's 3 we kee
 
 delta = 0.01
 
-total_frames = 10000
-
 file_location = repo.working_tree_dir+"/input_data/test1.csv"
 
-df = pd.read_csv(file_location)[::skip_frames].reset_index()
+df = pd.read_csv(file_location)[::skip_frames].sort_values(by=['frame']).reset_index()
+
+total_frames = len(df)
 
 time_passed=df[' timeExp']
 
@@ -30,8 +30,17 @@ def str2bool(v):
 
 grabbed=np.vectorize(str2bool)(df['grabbed'])
 
+is_target=df[' isTarget']
 
-fig = plt.figure()
+subject_no = df[' sub'][0]
+
+session_no = df[' session'][0]
+
+practice_bool = True if df['practice'][0]=='practice' else False
+
+window_title="Subject: "+str(subject_no)+" | Session: "+str(session_no)+" | Practice: "+str(practice_bool)
+
+fig = plt.figure(window_title)
 ax = p3.Axes3D(fig)
 
 plots = {}
@@ -39,6 +48,16 @@ plots = {}
 plots["head_eye"] = Scatter(np.array([df['head_x'],df['eye_x']]),np.array([df['head_y'],df['eye_y']]),np.array([df['head_z'],df['eye_z']]),ax,".","--","head_eye")
 
 plots["controller"] = Arrow(df[' controller_x'],df[' controller_y'],df[' controller_z'],df['controller_yaw'],df[' controller_pitch'],df[' controller_roll'],0.1,"black", "controller",ax,delta)
+
+max_x, min_x, max_y, min_y, max_z, min_z = 0, 0, 0, 0, 0, 0
+
+max_x = max(max(df['head_x']),max(df['eye_x']),max(df[' controller_x']))
+max_y = max(max(df['head_y']),max(df['eye_y']),max(df[' controller_y']))
+max_z = max(max(df['head_z']),max(df['eye_z']),max(df[' controller_z']))
+
+min_x = min(min(df['head_x']),min(df['eye_x']),min(df[' controller_x']))
+min_y = min(min(df['head_y']),min(df['eye_y']),min(df[' controller_y']))
+min_z = min(min(df['head_z']),min(df['eye_z']),min(df[' controller_z']))
 
 txt = fig.suptitle('')
 
@@ -48,8 +67,10 @@ def update_points(t):
 
     plots["head_eye"].update(t)
 
-    if grabbed[t]:
+    if grabbed[t] and is_target[t]:
         plots["controller"].update(t,color="orange")
+    elif grabbed[t]:
+        plots["controller"].update(t,color="red")
     else:
         plots["controller"].update(t,color="black")
 
@@ -70,4 +91,7 @@ ax.legend()
 ax.set_xlabel("x")
 ax.set_ylabel("y")
 ax.set_zlabel("z")
+ax.set_xlim(min_x,max_x)
+ax.set_ylim(min_y,max_y)
+ax.set_zlim(min_z,max_z)
 plt.show()
