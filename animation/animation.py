7554c754bad8c68228967060a17ab92ab09d74e8
Sebastian <sebastianfoulger@hotmail.co.uk>
2023-08-31 16:16:08 +0100

    Code improvement changes

--- animation/animation.py
+++ animation/animation.py
@@ -3,66 +3,111 @@ from matplotlib import animation
 import pandas as pd
 from matplotlib import pyplot as plt
 import os
-os.environ["GIT_PYTHON_REFRESH"] = "quiet"
 import git
 import time
 
 from plottable_objects import *
 
+os.environ['GIT_PYTHON_REFRESH'] = 'quiet'
+
 class AniObject:
-    def __init__(self, df):
+    """
+    Object for animation runs of the task
+    """    
+    def __init__(self, df: pd.DataFrame):
+        """Initialiser function for class
+
+        Args:
+            df (pd.DataFrame): dataframe with data for animation.
+        """        
         self.df = df.sort_values(by=['frame']).reset_index(drop=True)
 
         self.subject_no = df[' sub'][0]
 
         self.session_no = df[' session'][0]
 
-        practice_bool = True if df['practice'][0]=='practice' else False
+        practice_bool = df['practice'][0]=='practice'
 
-        window_title="Subject: "+str(self.subject_no)+" | Session: "+str(self.session_no)
-        window_title+=" | Practice: "+str(practice_bool)
+        window_title='Subject: '+str(self.subject_no)+' | Session: '+str(self.session_no)
+        window_title+=' | Practice: '+str(practice_bool)
 
         self.fig = plt.figure(window_title)
 
         self.ax = p3.Axes3D(self.fig)
-        self.ax.set_xlabel("x")
-        self.ax.set_ylabel("y")
-        self.ax.set_zlabel("z")
-        
+        self.ax.set_xlabel('x')
+        self.ax.set_ylabel('y')
+        self.ax.set_zlabel('z')
 
-    def animate(self, fps=10, skip_frames = 1, fps_bool = False, speed=1, arrow_length=0.3):
+    """
+    Public Functions
+    """
+
+    def animate(self, fps: int = 10, skip_frames: int = 1, fps_bool: bool = False, 
+                speed: int = 1, arrow_length: float = 0.3):
+        """Function to call for animation.
+
+        Args:
+            fps (int, optional): frames per second for animation. Defaults to 10.
+            skip_frames (int, optional): one in every skip_frames frames are kept. Only used in fps_bool is True. 
+            Defaults to 1.
+            fps_bool (bool, optional): set to True if speed of animation should be based on FPS. Set to False if speed
+            should be in accordance with real time. Defaults to False.
+            speed (int, optional): speed of animation multiplier. Only used if fps_bool is False. Defaults to 1.
+            arrow_length (float, optional): length of arrow. Defaults to 0.3.
+        """        
 
         delta = 0.01
 
-        ani_df = self.df[::skip_frames].reset_index(drop=True)
+        ani_df = self.df[::skip_frames].reset_index(drop=True).copy()
         total_frames = len(ani_df)
 
-        ani_df['frame_time'] = ani_df[' timeExp'].diff()
+        frame_time = ani_df[' timeExp'].diff()
+        ani_df['frame_time'] = frame_time
 
-        avg_frame_interval = 1000*sum(ani_df[(ani_df['frame_time'].notnull()) 
-                                        & (ani_df['frame_time']<skip_frames*0.02)]['frame_time'])/total_frames
+        if fps_bool:
+            _index = (frame_time.notnull()) & (frame_time<skip_frames*0.02)
+            summand = ani_df[_index]['frame_time']
+            avg_frame_interval = 1000*sum(summand)/total_frames
 
-        interval = round(avg_frame_interval/speed)
+            interval = round(avg_frame_interval/speed)
+        else:
+            interval = int(1000/fps)
 
         self.time_passed=ani_df[' timeExp']
 
-        self.grabbed=np.vectorize(lambda v: v.lower().strip() in ("yes", "true", "t", "1"))(ani_df['grabbed'])
+        is_grabbing = lambda v: v.lower().strip() in ('yes', 'true', 't', '1')
+
+        self.grabbed=np.vectorize(is_grabbing)(ani_df['grabbed'])
 
         self.is_target=ani_df[' isTarget']
 
         self.plots = {}
 
-        self.plots["head_eye"] = Scatter(xss=np.array([ani_df['head_x'],ani_df['eye_x']]),
+        self.plots['head_eye'] = Scatter(xss=np.array([ani_df['head_x'],ani_df['eye_x']]),
                                          yss=np.array([ani_df['head_y'],ani_df['eye_y']]),
                                          zss=np.array([ani_df['head_z'],ani_df['eye_z']]),
-                                         ax=self.ax,init_marker=".",init_linestyle="--",init_label="head_eye")
+                                         ax=self.ax,init_marker='.',init_linestyle='--',init_label='head_eye')
 
-        self.plots["controller"] = Arrow(xs=ani_df[' controller_x'],ys=ani_df[' controller_y'],
+        self.plots['controller'] = Arrow(xs=ani_df[' controller_x'],ys=ani_df[' controller_y'],
                                          zs=ani_df[' controller_z'],yaws=ani_df['controller_yaw'],
                                          pitches=ani_df[' controller_pitch'],rolls=ani_df[' controller_roll'],
-                                         init_length=arrow_length,init_color="black", init_label="controller",
+                                         init_length=arrow_length,init_color='black', init_label='controller',
                                          ax=self.ax,delta=delta)
         
+        workspace_x_mid, workspace_y_mid, workspace_z_mid = -0.05, -2.25, 2
+        resource_x_mid, resource_y_mid, resource_z_mid = 1, -0.188, -0.618
+        model_x_mid, model_y_mid, model_z_mid = -0.841, 0,-1.621
+
+        self.plots['workspace'] = self.ax.plot_surface(*self._generate_plane(workspace_x_mid,
+                                    workspace_y_mid,workspace_z_mid,dimensions = (1,1), orientation='y'), color='black')
+
+        self.plots['resource'] = self.ax.plot_surface(*self._generate_plane(resource_x_mid,
+                                    resource_y_mid,resource_z_mid,dimensions = (1,1), orientation='y'), color='black')
+        
+        self.plots['model'] = self.ax.plot_surface(*self._generate_plane(model_x_mid,
+                                    model_y_mid,model_z_mid,dimensions = (1,1), orientation='z'), color='black')
+
+
         max_x = max(max(ani_df['head_x']),max(ani_df['eye_x']),max(ani_df[' controller_x']))
         max_y = max(max(ani_df['head_y']),max(ani_df['eye_y']),max(ani_df[' controller_y']))
         max_z = max(max(ani_df['head_z']),max(ani_df['eye_z']),max(ani_df[' controller_z']))
@@ -71,40 +116,48 @@ class AniObject:
         min_y = min(min(ani_df['head_y']),min(ani_df['eye_y']),min(ani_df[' controller_y']))
         min_z = min(min(ani_df['head_z']),min(ani_df['eye_z']),min(ani_df[' controller_z']))
 
-        self.txt = self.fig.suptitle('')
+        self.txt = self.fig.suptitle('', x=0.25)
 
-        self.fig.canvas.mpl_connect('key_press_event', self.on_press)
+        self.fig.canvas.mpl_connect('key_press_event', self._on_press)
         self.start = time.time()
         self.start1 = self.time_passed[0]
-        if fps_bool:
-            self.ani=animation.FuncAnimation(self.fig, self.update_points, frames=total_frames, 
-                                                                                            interval = int(1000/fps))
-        else:
-            self.ani=animation.FuncAnimation(self.fig, self.update_points, frames=total_frames, interval = interval)
+
+        self.ani=animation.FuncAnimation(self.fig, self._update_animation, frames=total_frames, interval = interval)
         self.ani.running = True
 
         self.ax.legend()
         self.ax.set_xlim(min_x,max_x)
         self.ax.set_ylim(min_y,max_y)
         self.ax.set_zlim(min_z,max_z)
+
         plt.show()
 
-    def update_points(self,t):
-        
-        self.txt.set_text(f'''time passed={int(self.time_passed[t])} seconds,
+    """
+    Private Functions
+    """
+
+    def _update_animation(self,t: int):
+        """Function to call for animation update
+
+        Args:
+            t (int): frame of animation
+        """        
+        self.txt.set_text(f'''study time={int(self.time_passed[t])} seconds,
                             actual time passed={int(time.time()-self.start)}''')
 
-        self.plots["head_eye"].update(t)
+        self.plots['head_eye'].update(t)
 
         if self.grabbed[t] and self.is_target[t]:
-            self.plots["controller"].update(t,color="orange")
+            color = 'orange'
         elif self.grabbed[t]:
-            self.plots["controller"].update(t,color="red")
+            color = 'red'
         else:
-            self.plots["controller"].update(t,color="black")
+            color = 'black'
+
+        self.plots['controller'].update(t,color=color)
 
 
-    def on_press(self,event):
+    def _on_press(self,event):   
         if event.key.isspace():
             if self.ani.running:
                 self.ani.event_source.stop()
@@ -112,11 +165,50 @@ class AniObject:
                 self.ani.event_source.start()
             self.ani.running ^= True
 
+    def _generate_plane(self, x_mid: float, y_mid: float, z_mid: float, dimensions: tuple, orientation: str):
+        """Function to generate coordintes for plane plotting
+
+        Args:
+            x_mid (float): middle x-coordinate.
+            y_mid (float): middle y-coordinate.
+            z_mid (float): middle z-coordinate.
+            dimensions (tuple): 2-tuple with dimensions for the two non-flat directions.
+            orientation (str): the flat coordinate, i.e. if orientation='z' then the plane will be flat in the 'z'
+            plane.
+
+        Returns:
+            tuple: x, y, z coordinates for plane.
+        """        
+        if orientation=='x':
+            one_mid=y_mid
+            two_mid=z_mid
+            three_mid=x_mid
+        elif orientation=='y':
+            one_mid=x_mid
+            two_mid=z_mid
+            three_mid=y_mid
+        else:
+            one_mid=x_mid
+            two_mid=y_mid
+            three_mid=z_mid
+        one = [[one_mid-dimensions[0]/2,one_mid+dimensions[0]/2],[one_mid-dimensions[0]/2,one_mid+dimensions[0]/2]]
+
+        two = [[two_mid-dimensions[1]/2,two_mid-dimensions[1]/2],[two_mid+dimensions[1]/2,two_mid+dimensions[1]/2]]
+
+        three = [[three_mid,three_mid],[three_mid,three_mid]]
+        
+        if orientation=='x':
+            return three,one,np.array(two)
+        elif orientation=='y':
+            return one,three,np.array(two)
+        else:
+            return one,two,np.array(three)
+
 repo = git.Repo('.', search_parent_directories=True)
 repo.working_tree_dir
 
-file_location = repo.working_tree_dir+"/input_data/test1.csv"
+file_location = repo.working_tree_dir+'/input_data/test1.csv'
 df = pd.read_csv(file_location)
 
 obj = AniObject(df=df)
-obj.animate()
+obj.animate(skip_frames=10, speed=5)
