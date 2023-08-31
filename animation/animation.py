f0b8927cc320d280687867c4df3a0144427935b4
Sebastian <sebastianfoulger@hotmail.co.uk>
2023-08-31 12:11:05 +0100

    OOP-ified animations + some small changes

--- animation/animation.py
+++ animation/animation.py
@@ -5,93 +5,118 @@ from matplotlib import pyplot as plt
 import os
 os.environ["GIT_PYTHON_REFRESH"] = "quiet"
 import git
+import time
 
 from plottable_objects import *
+
+class AniObject:
+    def __init__(self, df):
+        self.df = df.sort_values(by=['frame']).reset_index(drop=True)
+
+        self.subject_no = df[' sub'][0]
+
+        self.session_no = df[' session'][0]
+
+        practice_bool = True if df['practice'][0]=='practice' else False
+
+        window_title="Subject: "+str(self.subject_no)+" | Session: "+str(self.session_no)
+        window_title+=" | Practice: "+str(practice_bool)
+
+        self.fig = plt.figure(window_title)
+
+        self.ax = p3.Axes3D(self.fig)
+        self.ax.set_xlabel("x")
+        self.ax.set_ylabel("y")
+        self.ax.set_zlabel("z")
+        
+
+    def animate(self, fps=10, skip_frames = 1, fps_bool = False, speed=1, arrow_length=0.3):
+
+        delta = 0.01
+
+        ani_df = self.df[::skip_frames].reset_index(drop=True)
+        total_frames = len(ani_df)
+
+        ani_df['frame_time'] = ani_df[' timeExp'].diff()
+
+        avg_frame_interval = 1000*sum(ani_df[(ani_df['frame_time'].notnull()) 
+                                        & (ani_df['frame_time']<skip_frames*0.02)]['frame_time'])/total_frames
+
+        interval = round(avg_frame_interval/speed)
+
+        self.time_passed=ani_df[' timeExp']
+
+        self.grabbed=np.vectorize(lambda v: v.lower().strip() in ("yes", "true", "t", "1"))(ani_df['grabbed'])
+
+        self.is_target=ani_df[' isTarget']
+
+        self.plots = {}
+
+        self.plots["head_eye"] = Scatter(xss=np.array([ani_df['head_x'],ani_df['eye_x']]),
+                                         yss=np.array([ani_df['head_y'],ani_df['eye_y']]),
+                                         zss=np.array([ani_df['head_z'],ani_df['eye_z']]),
+                                         ax=self.ax,init_marker=".",init_linestyle="--",init_label="head_eye")
+
+        self.plots["controller"] = Arrow(xs=ani_df[' controller_x'],ys=ani_df[' controller_y'],
+                                         zs=ani_df[' controller_z'],yaws=ani_df['controller_yaw'],
+                                         pitches=ani_df[' controller_pitch'],rolls=ani_df[' controller_roll'],
+                                         init_length=arrow_length,init_color="black", init_label="controller",
+                                         ax=self.ax,delta=delta)
+        
+        max_x = max(max(ani_df['head_x']),max(ani_df['eye_x']),max(ani_df[' controller_x']))
+        max_y = max(max(ani_df['head_y']),max(ani_df['eye_y']),max(ani_df[' controller_y']))
+        max_z = max(max(ani_df['head_z']),max(ani_df['eye_z']),max(ani_df[' controller_z']))
+
+        min_x = min(min(ani_df['head_x']),min(ani_df['eye_x']),min(ani_df[' controller_x']))
+        min_y = min(min(ani_df['head_y']),min(ani_df['eye_y']),min(ani_df[' controller_y']))
+        min_z = min(min(ani_df['head_z']),min(ani_df['eye_z']),min(ani_df[' controller_z']))
+
+        self.txt = self.fig.suptitle('')
+
+        self.fig.canvas.mpl_connect('key_press_event', self.on_press)
+        self.start = time.time()
+        self.start1 = self.time_passed[0]
+        if fps_bool:
+            self.ani=animation.FuncAnimation(self.fig, self.update_points, frames=total_frames, 
+                                                                                            interval = int(1000/fps))
+        else:
+            self.ani=animation.FuncAnimation(self.fig, self.update_points, frames=total_frames, interval = interval)
+        self.ani.running = True
+
+        self.ax.legend()
+        self.ax.set_xlim(min_x,max_x)
+        self.ax.set_ylim(min_y,max_y)
+        self.ax.set_zlim(min_z,max_z)
+        plt.show()
+
+    def update_points(self,t):
+        
+        self.txt.set_text(f'''time passed={int(self.time_passed[t])} seconds,
+                            actual time passed={int(time.time()-self.start)}''')
+
+        self.plots["head_eye"].update(t)
+
+        if self.grabbed[t] and self.is_target[t]:
+            self.plots["controller"].update(t,color="orange")
+        elif self.grabbed[t]:
+            self.plots["controller"].update(t,color="red")
+        else:
+            self.plots["controller"].update(t,color="black")
+
+
+    def on_press(self,event):
+        if event.key.isspace():
+            if self.ani.running:
+                self.ani.event_source.stop()
+            else:
+                self.ani.event_source.start()
+            self.ani.running ^= True
 
 repo = git.Repo('.', search_parent_directories=True)
 repo.working_tree_dir
 
-fps = 1000 # Frames per second for animation
-
-skip_frames = 1 # We keep one in every skip_frames frames. i.e. if it's 3 we keep 1/3 frames. The idea is to be able to simulate faster without visibly losing data
-
-delta = 0.01
-
 file_location = repo.working_tree_dir+"/input_data/test1.csv"
+df = pd.read_csv(file_location)
 
-df = pd.read_csv(file_location)[::skip_frames].sort_values(by=['frame']).reset_index()
-
-total_frames = len(df)
-
-time_passed=df[' timeExp']
-
-def str2bool(v):
-  return v.lower().strip() in ("yes", "true", "t", "1")
-
-grabbed=np.vectorize(str2bool)(df['grabbed'])
-
-is_target=df[' isTarget']
-
-subject_no = df[' sub'][0]
-
-session_no = df[' session'][0]
-
-practice_bool = True if df['practice'][0]=='practice' else False
-
-window_title="Subject: "+str(subject_no)+" | Session: "+str(session_no)+" | Practice: "+str(practice_bool)
-
-fig = plt.figure(window_title)
-ax = p3.Axes3D(fig)
-
-plots = {}
-
-plots["head_eye"] = Scatter(np.array([df['head_x'],df['eye_x']]),np.array([df['head_y'],df['eye_y']]),np.array([df['head_z'],df['eye_z']]),ax,".","--","head_eye")
-
-plots["controller"] = Arrow(df[' controller_x'],df[' controller_y'],df[' controller_z'],df['controller_yaw'],df[' controller_pitch'],df[' controller_roll'],0.1,"black", "controller",ax,delta)
-
-max_x, min_x, max_y, min_y, max_z, min_z = 0, 0, 0, 0, 0, 0
-
-max_x = max(max(df['head_x']),max(df['eye_x']),max(df[' controller_x']))
-max_y = max(max(df['head_y']),max(df['eye_y']),max(df[' controller_y']))
-max_z = max(max(df['head_z']),max(df['eye_z']),max(df[' controller_z']))
-
-min_x = min(min(df['head_x']),min(df['eye_x']),min(df[' controller_x']))
-min_y = min(min(df['head_y']),min(df['eye_y']),min(df[' controller_y']))
-min_z = min(min(df['head_z']),min(df['eye_z']),min(df[' controller_z']))
-
-txt = fig.suptitle('')
-
-def update_points(t):
-
-    txt.set_text('time passed={:d} seconds'.format(int(time_passed[t])))
-
-    plots["head_eye"].update(t)
-
-    if grabbed[t] and is_target[t]:
-        plots["controller"].update(t,color="orange")
-    elif grabbed[t]:
-        plots["controller"].update(t,color="red")
-    else:
-        plots["controller"].update(t,color="black")
-
-
-def on_press(event):
-    if event.key.isspace():
-        if ani.running:
-            ani.event_source.stop()
-        else:
-            ani.event_source.start()
-        ani.running ^= True
-
-fig.canvas.mpl_connect('key_press_event', on_press)
-ani=animation.FuncAnimation(fig, update_points, frames=total_frames, interval = int(1000/fps))
-ani.running = True
-
-ax.legend()
-ax.set_xlabel("x")
-ax.set_ylabel("y")
-ax.set_zlabel("z")
-ax.set_xlim(min_x,max_x)
-ax.set_ylim(min_y,max_y)
-ax.set_zlim(min_z,max_z)
-plt.show()
+obj = AniObject(df=df)
+obj.animate()
--- animation/plottable_objects.py
+++ animation/plottable_objects.py
@@ -1,22 +1,48 @@
 import numpy as np
+import pandas as pd
 
-# Object for animated scatter plot of points:
 class Scatter:
-    def __init__(self, xss,yss,zss,ax, init_marker, init_linestyle, init_label):
+    """
+    Class to handle dynamic scatter plot (used for head-to-eye)
+    """    
+    def __init__(self, xss: np.array,yss: np.array,zss: np.array,
+                 ax, init_marker: str, init_linestyle: str, init_label: str):
+        """Initialiser function for class
+
+        Args:
+            xss (np.array): Numpy array where each entry is a pandas series containing the x-coordinates of one of the
+            scatter points. Each series must be the same length.
+            yss (np.array): As above but for y-coordinates. Must have the same length as above and the same length of
+            each series.
+            zss (np.array): As above but for z-coordinates.
+            ax (mpl_toolkits.mplot3d.axes3d.Axes3D): Axes for plotting.
+            init_marker (str): Marker for scatter points.
+            init_linestyle (str): Linestyle between scatter points.
+            init_label (str): Label for scatter.
+        """        
         self.xss=xss
         self.yss=yss
         self.zss=zss
-
         self.ax=ax
-
         self.prev_marker=init_marker
         self.prev_linestyle=init_linestyle
         self.prev_label=init_label
-     
-        self.points, = ax.plot(xss[:,0],yss[:,0],zss[:,0], marker=init_marker, linestyle=init_linestyle, label=init_label)
+        self.points, = ax.plot(xss[:,0],yss[:,0],zss[:,0], 
+                               marker=init_marker, linestyle=init_linestyle, label=init_label)
 
-    def update(self,t,marker=None,linestyle=None,label=None):
+    """Public functions"""
 
+    def update(self,t: int, marker: str = None, linestyle: str = None, label: str = None):
+        """Update function for scatter plot.
+
+        Args:
+            t (int): index of data to update with.
+            marker (str, optional): marker to update scatter points with. Set to None if no update is desired. Defaults 
+            to None.
+            linestyle (str, optional): linestyle to update scatter with. Set to None if no update is desired. Defaults 
+            to None.
+            label (str, optional): label to update scatter with. Set to None if no update is desired. Defaults to None.
+        """        
         if marker==None:
             marker=self.prev_marker
         else:
@@ -39,9 +65,28 @@ class Scatter:
         self.points.set_linestyle(linestyle)
         self.points.set_label(label)
 
-# Object for directional point
 class Arrow:
-    def __init__(self, xs,ys,zs,yaws,pitches,rolls,init_length,init_color,init_label, ax, delta):
+    """
+    Class to handle dynamic arrow (used for controller).
+    """    
+    def __init__(self, xs: pd.Series,ys: pd.Series,zs: pd.Series,
+                 yaws: pd.Series,pitches: pd.Series,rolls: pd.Series,
+                 init_length: float,init_color: str,init_label: str, ax, delta: float):
+        """Initialiser function for class.
+
+        Args:
+            xs (pd.Series): series of x-coordinates for base of arrow.
+            ys (pd.Series): series of y-coordinates for base of arrow.
+            zs (pd.Series): series of z-coordinates for base of arrow.
+            yaws (pd.Series): series of yaws for arrow direction.
+            pitches (pd.Series): series of pitches for arrow direction.
+            rolls (pd.Series): series of rolls for arrow direction.
+            init_length (float): initial length of arrow.
+            init_color (str): initial colour of arrow.
+            init_label (str): initial name for arrow.
+            ax (mpl_toolkits.mplot3d.axes3d.Axes3D): axes for plotting.
+            delta (float): size of arrow in calculation.
+        """        
         self.xs=xs
         self.ys=ys
         self.zs=zs
@@ -55,12 +100,22 @@ class Arrow:
         self.prev_color = init_color
         self.prev_label = init_label
 
-        self.quiver = ax.quiver(*self.get_arrow(xs[0], ys[0],zs[0],yaws[0],pitches[0],rolls[0]), length=init_length, color=init_color, label=init_label)
+        self.quiver = ax.quiver(*self._get_arrow(xs[0], ys[0],zs[0],yaws[0],pitches[0],rolls[0]), length=init_length, 
+                                color=init_color, label=init_label)
 
-    def get_arrow(self,x,y,z,yaw,pitch,roll):
-        return x,y,z,x+self.delta*np.cos(yaw)*np.cos(pitch),y+self.delta*np.sin(yaw)*np.cos(pitch),z+self.delta*np.sin(pitch)
+    """Public functions"""
 
-    def update(self,t,length=None, color=None, label=None):
+    def update(self,t: int,length: float = None, color: str = None, label: str = None):
+        """Update function for arrow.
+
+        Args:
+            t (int): index of data to update with.
+            length (float, optional): length of arrow to update with. Set to None if no update is desired. Defaults to 
+            None.
+            color (str, optional): colour of arrow to update with. Set to None if no update is desired. Defaults to 
+            None.
+            label (str, optional): name of arrow to update with. Set to None if no update is desired. Defaults to None.
+        """        
         if length==None:
             length=self.prev_length
         else:
@@ -77,4 +132,26 @@ class Arrow:
             self.prev_label=label
 
         self.quiver.remove()
-        self.quiver = self.ax.quiver(*self.get_arrow(self.xs[t], self.ys[t], self.zs[t], self.yaws[t],self.pitches[t],self.rolls[t]), length=length, color=color, label=label)
+        self.quiver = self.ax.quiver(*self._get_arrow(self.xs[t], self.ys[t], self.zs[t], self.yaws[t],self.pitches[t],
+                                                      self.rolls[t]), length=length, color=color, label=label)
+
+    """Private functions"""
+
+    def _get_arrow(self,x: float,y: float,z: float,yaw: float,pitch: float,roll: float):
+        """_summary_
+
+        Args:
+            x (float): _description_
+            y (float): _description_
+            z (float): _description_
+            yaw (float): _description_
+            pitch (float): _description_
+            roll (float): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        arrow_return = (x,y,z,x+self.delta*np.cos(yaw)*np.cos(pitch),
+                        y+self.delta*np.sin(yaw)*np.cos(pitch),z+self.delta*np.sin(pitch))
+        return arrow_return
+    
